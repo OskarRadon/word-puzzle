b473fc99aecddfd9cd064e2ef44947f7480f5047
Afton Downey and Oskar Radon <aaike001@gmail.com>
2016-01-21 10:51:06 -0800

    Add global var comment

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+
 var puzzle = function(inputString) {
-  return inputString.split(" ");
+
+  var splitString = inputString.split(" ");
+  console.log(inputString.replace(/[aeiou]/gi, '-'));
+
+  // splitString.forEach(word) {
+  //   var vowels = word.search(/([aeiouAEIOU]+)/g);
+  //   return vowels.replace("-")
+  // };
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,9 @@
 describe('puzzle', function() {
   it("split input string into an array", function() {
-    expect(puzzle("Hello there")).to.eql(["Hello", "there"])
+    expect(puzzle("Hello there")).to.eql(["Hello", "there"]);
+  });
+
+  it("finds all vowels and replaces them with '-'", function(){
+    expect(puzzle("Hello there")).to.eql(["H-ll-", "th-r-"]);
   });
 });
